ab0e91f2163879aa9bfa771c4a6a7325c5164f7c
nishadchittangadan <59539337+nishadchittangadan@users.noreply.github.com>
2021-07-11 01:34:30 +0300

    Initial commit

---
+++ src/com/company/SimpleCalculator.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+    public double getAdditionResult(){
+        return getFirstNumber() + getSecondNumber();
+    }
+    public double getSubtractionResult(){
+        double result = getFirstNumber() - getSecondNumber();
+        return result;
+    }
+    public double getMultiplicationResult(){
+        return getFirstNumber() * getSecondNumber();
+    }
+    public double getDivisionResult(){
+        if(getFirstNumber()==0 || getSecondNumber() == 0){
+            return 0;
+        }
+        return getFirstNumber()/getSecondNumber();
+    }
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+}
